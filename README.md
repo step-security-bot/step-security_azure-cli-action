2cf3538b57cf0eeaa80bf1060ba7feb3adf76acd
Anurag Rajawat <anurag@stepsecurity.io>
2026-09-16 12:57:30 +0530

    feat: onboard azure/cli action
    Signed-off-by: Anurag Rajawat <anurag@stepsecurity.io>

--- README.md
+++ README.md
@@ -1,1 +1,98 @@
-# azure-cli-action
+[![StepSecurity Maintained Action](https://raw.githubusercontent.com/step-security/maintained-actions-assets/main/assets/maintained-action-banner.png)](https://docs.stepsecurity.io/actions/stepsecurity-maintained-actions)
+
+# GitHub Action for Azure CLI
+
+> [!WARNING]
+> Starting with Azure CLI version `2.64.0`, the Azure CLI docker image will be based on Azure Linux. The `az` commands are not affected by this change. To ensure the compatibility of your pipelines, please migrate Alpine-specific commands to Azure Linux commands in the scripts used in the step-security/azure-cli-action.
+> 
+> For more information, see https://go.microsoft.com/fwlink/?linkid=2282203.
+
+With Azure CLI GitHub Action, you can automate your workflow by executing [Azure CLI](https://github.com/Azure/azure-cli) commands to manage Azure resources inside of an Action.
+
+The action executes the Azure CLI Bash script on a user defined Azure CLI version. If the user does not specify a version, the version of Azure CLI installed on the agent is used. If there is no version of Azure CLI found on the agent, the action falls back the version to `latest`.
+Read more about various Azure CLI versions [here](https://github.com/Azure/azure-cli/releases).
+
+- `azcliversion` – **Optional** Example: 2.30.0, Default: set to az cli version of the agent.
+- `inlineScript` – **Required**
+
+Azure CLI GitHub Action is supported for the Azure public cloud as well as Azure government clouds ('AzureUSGovernment' or 'AzureChinaCloud') and Azure Stack ('AzureStack') Hub. Before running this action, login to the respective Azure Cloud  using [Azure Login](https://github.com/Azure/login) by setting appropriate value for the `environment` parameter.
+
+The definition of this GitHub Action is in [action.yml](https://github.com/step-security/azure-cli-action/blob/main/action.yml).  The action status is determined by the exit code returned by the script rather than StandardError stream.
+
+> [!NOTE]
+> Please note that the action executes Azure CLI script in a docker container. This means that the action is subjected to potential restrictions which arise from containerized execution. For example:
+> 1. If script sets up an environment variable, it will not take effect in host and hence subsequent actions shouldn't rely on such environment variable.
+> 2. There is some restriction on how cross action file read/write is done. `GITHUB_WORKSPACE` directory in host is mapped to working directory inside container. So, if the action wants to create a file, which will be read by subsequent actions, it should do so within current working directory tree.
+
+> [!WARNING]
+> By default, the output of Azure CLI commands is printed to the stdout stream. Without redirecting the stdout stream, contents in it will be stored in the build log of the action. Configure Azure CLI to _not_ show output in the console screen or print in the log by setting the environment variable `AZURE_CORE_OUTPUT` to `none`. If you need the output of a specific command, override the default setting using the argument `--output` with your format of choice. For more information on output options with the Azure CLI, see [Format output](https://learn.microsoft.com/cli/azure/format-output-azure-cli).
+
+## Sample workflow
+
+### Dependencies on other GitHub Actions
+* [Azure Login](https://github.com/Azure/login) – **Optional** Login with your Azure credentials, required only for authentication via Azure credentials. If you use this action, make sure to either use the default value of `azcliversion` or `azcliversion >= 2.30.0` for all the workflows. Authentication via connection strings or keys do not require this step.
+* [Checkout](https://github.com/actions/checkout) – **Optional** To execute the scripts present in your repository.
+
+### Workflow to execute an Azure CLI script of the latest Azure CLI version
+```yaml
+# File: .github/workflows/workflow.yml
+
+on: [push]
+
+name: AzureCLISample
+
+jobs:
+
+  build-and-deploy:
+    runs-on: ubuntu-latest
+    steps:
+
+    - name: Azure Login
+      uses: step-security/azure-login@v3
+      with:
+        creds: ${{ secrets.AZURE_CREDENTIALS }}
+
+    - name: Azure CLI script
+      uses: step-security/azure-cli-action@v2
+      with:
+        azcliversion: latest
+        inlineScript: |
+          az account show
+          az storage -h
+```
+
+### Workflow to execute an Azure CLI script of a specific CLI version via file present in your repository.
+```yaml
+# File: .github/workflows/workflowForFile.yml
+
+on: [push]
+
+name: AzureCLISampleForFile
+
+jobs:
+
+  build-and-deploy:
+    runs-on: ubuntu-latest
+    steps:
+
+    - name: Azure Login
+      uses: step-security/azure-login@v3
+      with:
+        creds: ${{ secrets.AZURE_CREDENTIALS }}
+
+    - name: Checkout
+      uses: actions/checkout@v7
+
+    - name: Azure CLI script file
+      uses: step-security/azure-cli-action@v2
+      with:
+        azcliversion: 2.30.0
+        inlineScript: |
+          chmod +x $GITHUB_WORKSPACE/sampleScript.sh
+          $GITHUB_WORKSPACE/sampleScript.sh
+```
+* [`GITHUB_WORKSPACE`](https://docs.github.com/actions/using-github-hosted-runners/about-github-hosted-runners/about-github-hosted-runners#file-systems) is the environment variable provided by GitHub which represents the root of your repository.
+
+# Getting Help for Azure CLI Issues
+
+If you encounter an issue related to the Azure CLI commands executed in your script, you can file an issue directly on the [Azure CLI repository](https://github.com/Azure/azure-cli/issues/new/choose).
---
+++ src/main.ts
@@ -0,0 +1,167 @@
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as io from '@actions/io';
+import * as os from 'os';
+import * as path from 'path';
+import * as crypto from 'crypto';
+const util = require('util');
+const cpExec = util.promisify(require('child_process').exec);
+
+import { createScriptFile, TEMP_DIRECTORY, NullOutstreamStringWritable, deleteFile, getCurrentTime, checkIfEnvironmentVariableIsOmitted } from './utils';
+import {validateSubscription} from "./subscription";
+
+const START_SCRIPT_EXECUTION_MARKER: string = "Starting script execution via docker image mcr.microsoft.com/azure-cli:";
+const AZ_CLI_VERSION_DEFAULT_VALUE = 'agentazcliversion'
+const prefix = !!process.env.AZURE_HTTP_USER_AGENT ? `${process.env.AZURE_HTTP_USER_AGENT}` : "";
+const AZ_CLI_TAG_LIST_URL = "https://mcr.microsoft.com/v2/azure-cli/tags/list";
+
+export async function main() {
+    await validateSubscription();
+
+    let usrAgentRepo = crypto.createHash('sha256').update(`${process.env.GITHUB_REPOSITORY}`).digest('hex');
+    let actionName = 'AzureCLIAction';
+    let userAgentString = (!!prefix ? `${prefix}+` : '') + `GITHUBACTIONS/${actionName}@v2_${usrAgentRepo}_${process.env.RUNNER_ENVIRONMENT}_${process.env.GITHUB_RUN_ID}`;
+    process.env.AZURE_HTTP_USER_AGENT = userAgentString;
+
+    var scriptFileName: string = '';
+    const CONTAINER_NAME = `MICROSOFT_AZURE_CLI_${getCurrentTime()}_CONTAINER`;
+    try {
+        if (process.env.RUNNER_OS != 'Linux') {
+            throw new Error('Please use Linux-based OS as a runner.');
+        }
+
+        let inlineScript: string = core.getInput('inlineScript', { required: true });
+        let azcliversion: string = core.getInput('azcliversion', { required: false }).trim().toLowerCase();
+
+        if (azcliversion == AZ_CLI_VERSION_DEFAULT_VALUE) {
+            try {
+                const { stdout } = await cpExec('az version');
+                azcliversion = JSON.parse(stdout)["azure-cli"]
+            } catch (err) {
+                console.log('Failed to fetch az cli version from agent. Reverting back to latest.')
+                azcliversion = 'latest'
+            }
+        }
+
+        if (!(await checkIfValidCLIVersion(azcliversion))) {
+            core.error('Please enter a valid azure cli version. \nSee available versions: https://github.com/Azure/azure-cli/releases');
+            throw new Error('Please enter a valid azure cli version. \nSee available versions: https://github.com/Azure/azure-cli/releases')
+        }
+
+        if (!inlineScript.trim()) {
+            core.error('Please enter a valid script.');
+            throw new Error('Please enter a valid script.')
+        }
+        inlineScript = ` set -e >&2; echo '${START_SCRIPT_EXECUTION_MARKER}' >&2; ${inlineScript}`;
+        scriptFileName = await createScriptFile(inlineScript);
+
+        const hostAzureConfigDir = process.env.AZURE_CONFIG_DIR || path.join(process.env.HOME, '.azure');
+        const containerAzureConfigDir = '/root/.azure';
+
+        /*
+        For the docker run command, we are doing the following
+        - Set the working directory for docker continer
+        - volume mount the GITHUB_WORKSPACE env variable (path where users checkout code is present) to work directory of container
+        - volume mount Azure config directory between host and container,
+        - volume mount temp directory between host and container, inline script file is created in temp directory
+        */
+        let args: string[] = ["run", "--workdir", `${process.env.GITHUB_WORKSPACE}`,
+            "-v", `${process.env.GITHUB_WORKSPACE}:${process.env.GITHUB_WORKSPACE}`,
+            "-v", `${hostAzureConfigDir}:${containerAzureConfigDir}`,
+            "-v", `${TEMP_DIRECTORY}:${TEMP_DIRECTORY}`
+        ];
+        for (let key in process.env) {
+            if (!checkIfEnvironmentVariableIsOmitted(key) && process.env[key]) {
+                args.push("-e", `${key}=${process.env[key]}`);
+            }
+        }
+        args.push("--name", CONTAINER_NAME,
+            `mcr.microsoft.com/azure-cli:${azcliversion}`,
+            "bash", "--noprofile", "--norc", "-e", `${TEMP_DIRECTORY}/${scriptFileName}`);
+
+        console.log(`${START_SCRIPT_EXECUTION_MARKER}${azcliversion}`);
+        await executeDockerCommand(args);
+        console.log("az script ran successfully.");
+    }
+    catch (error) {
+        core.error(error);
+        throw error;
+    }
+    finally {
+        // clean up
+        const scriptFilePath: string = path.join(TEMP_DIRECTORY, scriptFileName);
+        await deleteFile(scriptFilePath);
+        console.log("cleaning up container...");
+        await executeDockerCommand(["rm", "--force", CONTAINER_NAME], true);
+    }
+};
+
+const AZCLI_VERSION_PATTERN = /^(latest|\d+\.\d+\.\d+)$/;
+
+const checkIfValidCLIVersion = async (azcliversion: string): Promise<boolean> => {
+    const allVersions: Array<string> = await getAllAzCliVersions();
+    if (!allVersions || allVersions.length == 0) {
+        return AZCLI_VERSION_PATTERN.test(azcliversion);
+    }
+    return allVersions.some((eachVersion) => eachVersion.toLowerCase() === azcliversion);
+}
+
+const getAllAzCliVersions = async (): Promise<Array<string>> => {
+    try {
+        const response = await fetch(AZ_CLI_TAG_LIST_URL);
+        if (!response.ok) {
+            const errorText = await response.text();
+            throw new Error(`HTTP error! status: ${response.status}, errorText: ${errorText}`);
+        }
+        const data = await response.json();
+        if (data && data.tags) {
+            return data.tags;
+        }
+        else {
+            throw new Error('Response data does not contain tags.');
+        }
+    }
+    catch (error) {
+        core.warning(`Unable to fetch all az cli versions with Error: ${error}. Skipping the version check.`);
+    }
+    return [];
+};
+
+const executeDockerCommand = async (args: string[], continueOnError: boolean = false): Promise<void> => {
+    const dockerTool: string = await io.which("docker", true);
+    var errorStream: string = '';
+    var shouldOutputErrorStream: boolean = false;
+    var execOptions: any = {
+        outStream: new NullOutstreamStringWritable({ decodeStrings: false }),
+        listeners: {
+            stdout: (data: any) => console.log(data.toString()), //to log the script output while the script is running.
+            errline: (data: string) => {
+                if (!shouldOutputErrorStream) {
+                    errorStream += data + os.EOL;
+                }
+                else {
+                    console.log(data);
+                }
+                if (data.trim() === START_SCRIPT_EXECUTION_MARKER) {
+                    shouldOutputErrorStream = true;
+                    errorStream = ''; // Flush the container logs. After this, script error logs will be tracked.
+                }
+            }
+        }
+    };
+    var exitCode;
+    try {
+        exitCode = await exec.exec(dockerTool, args, execOptions);
+    } catch (error) {
+        if (!continueOnError) {
+            throw error;
+        }
+        core.warning(error);
+    }
+    finally {
+        if (exitCode !== 0 && !continueOnError) {
+            throw new Error(errorStream || 'az cli script failed.');
+        }
+        core.warning(errorStream)
+    }
+}
